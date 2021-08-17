a48053ff7f178027440af5960b92838cf6f99d0f
icebreaker26 <alejandro.torres0826@gmail.com>
2021-08-17 09:15:24 -0500

    1.1

--- script.js
+++ script.js
@@ -7,17 +7,17 @@ function nombre()
   aleatorio = Math.floor(Math.random() * (20 - 1)) + 1;
 
   console.log(aleatorio);
+  console.log(texto)
+;  if (aleatorio <=10){
 
-  if (aleatorio <=10){
-
-alert("menor que 10");
+alert("No le gustas" +" "+ texto +" "+ ":(");
 
 
 };
 
   if (aleatorio >10){
 
-alert("mayor que 10");
+alert("¡¡¡LE GUSTAS!!!" +" " +  texto + " " + ";)");
 
 
 };
