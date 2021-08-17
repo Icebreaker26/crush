7e453cc7f17706433315f5b1dbe281242e129ff8
icebreaker26 <alejandro.torres0826@gmail.com>
2021-08-17 11:18:26 -0500

    1.3

--- script.js
+++ script.js
@@ -7,21 +7,28 @@ function nombre()
   aleatorio = Math.floor(Math.random() * (20 - 1)) + 1;
 
   console.log(aleatorio);
-  console.log(texto)
-;  if (aleatorio <=10){
+  console.log(texto);
 
-alert("No le gustas" +" "+ texto +" "+ ":(");
+    if (texto === "") {
+
+      console.log(texto);
+      alert("¡Hey! ¿Cual es tu nombre?");
+    }
+
+       else if (aleatorio <=10){
+
+         alert("No le gustas" +" "+ texto +" "+ ":(");
+
+       }
+
+       else if (aleatorio >10){
+
+         alert("¡¡¡LE GUSTAS!!!" +" " +  texto + " " + ";)");
+       }
+
 
 
 };
 
-  if (aleatorio >10){
 
-alert("¡¡¡LE GUSTAS!!!" +" " +  texto + " " + ";)");
-
-
-};
-
-
-
-}
+    document.getElementById("formulario").addEventListener('submit', nombre);
